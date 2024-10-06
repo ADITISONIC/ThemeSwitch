35f7574b63a105384a82b2e45f1ac70ab70cc8a2
ADITISONIC <aditipython@gmail.com>
2024-10-06 19:29:03 +0530

    changes made

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from "react";
-import 
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
 
 export default function App(){
     return(
-
+      <div className="container">
+        <Navbar/>
+        <Main/>
+      </div>
     )
 }
