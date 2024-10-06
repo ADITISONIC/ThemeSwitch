65986f9224067dd29357399e76360b2f24041f1b
ADITISONIC <aditipython@gmail.com>
2024-10-06 19:38:48 +0530

    Navbar added

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,16 @@
 import React from "react"
 
-export default function Navbar(){
-    reutrn(
-        
+export default function Navbar(props){
+    return(
+      <nav className={props.darkMode?"dark":""}>
+      <h3 className="text">React Facts</h3>
+      <div className="toggler">
+        <p className="light">Light</p>
+        <div className="slider" onClick={props.toggleDarkMode}>
+            <div className="slider-circle"></div>
+        </div>
+        <p className="dark"></p>
+      </div>
+      </nav>
     )
 }
