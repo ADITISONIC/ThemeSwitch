c8453c792e2e05e3b6ebbe0a41ce62e638472d3d
ADITISONIC <aditipython@gmail.com>
2024-10-06 19:41:52 +0530

    Main added

--- components/Main.js
+++ components/Main.js
@@ -1,7 +1,14 @@
 import React from "react";
 
-export default function Main(){
+export default function Main(props){
     return(
-        
+     <main className={props.darkMode?"dark":""}>
+      <h1 className="title">Fun facts</h1>
+      <ul className="facts">
+        <li>hi</li>
+        <li>hlo</li>
+        <li>hello</li>
+      </ul>
+     </main>
     )
 }
